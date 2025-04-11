5262b9061fdd7f401298c4b6765af5c057c8d96d
Eric Vasconcelos de Oliveira <ericvascon@gmail.com>
2025-04-10 22:00:26 -0300

    feat: create route to get user info and count of active lands

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, INestApplication } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import appConfig from './app/config/app.config';
 
@@ -6,6 +7,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   appConfig(app)
   await app.listen(process.env.PORT ?? 3000);
+}
+
+export function registerGlobals(app: INestApplication) {
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      strategy: 'excludeAll',
+      excludeExtraneousValues: true
+    }),
+  );
 }
 
 bootstrap().catch((err) => {
